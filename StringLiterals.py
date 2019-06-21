7f899cf112f81c4be11a10cd1c6070cd751a303d
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-21 10:09:00 +0300

    guessing and comparing strings from input of user,

--- StringLiterals.py
+++ StringLiterals.py
@@ -1,2 +1,24 @@
 
 #working with strings in python
+
+
+capture = input("Enter name : ")
+
+print(capture[1])
+
+defaultWord = "kamadi"
+
+print(defaultWord[1])
+
+if(capture[1] == defaultWord[1]):
+    print("Values of a ARE SECOND.")
+else:
+    print("You didng pass this men")
+
+
+
+
+
+
+
+
