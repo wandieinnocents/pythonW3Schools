bfbbcd93bcfa735f158ae95b9022ca6bfc5e2a19
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-21 09:16:03 +0300

    docstring python

--- Main.py
+++ Main.py
@@ -3,8 +3,19 @@
 
 
 def main():
+
     print("Hello world")
+
+    #testing a number
+    if(5>2):
+        print("5 IS GREATER THAN 2")
+
+    #documentation capability , doc strings
+    #single line / multiline docstring to document your code.
+
+    """" sample
+     documentation """
+
 
 
 if __name__ == '__main__':main()
-    
