e5cd8294c2c62dfb431339b541f838976b7ee6e5
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-21 09:31:59 +0300

    num formats, int, float, complex numbers

---
+++ Numbers.py
@@ -0,0 +1,21 @@
+# w3schools python learning material
+
+
+def main():
+
+    x = 10
+    y = 10.4
+    z = 2j
+
+    #pring and check formats
+    print("x is : {}".format(x))
+    print(type(x))
+
+    print("y is : {}".format(y))
+    print(type(y))
+
+    print("z is : {}".format(z))
+    print(type(z))
+
+
+if __name__ == '__main__': main()
