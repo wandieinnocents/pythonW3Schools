9cd903e3beecdb88450f27bf80206a06d5528b44
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-21 09:53:45 +0300

    testing casting for float..

--- CastingVariables.py
+++ CastingVariables.py
@@ -1,12 +1,32 @@
 
 #python variable casting
 
+#casting integers
 x = int(1)
 y = int(2.4)
 z = int(3)
 
 print(x)
+print(type(x))
+
 print(y)
+print(type(y))
+
 print(z)
+print(type(z))
+
+#casting floats
+a = float(1)
+b = float(1.2)
+c = float(-9.00)
+print(a)
+print(type(a))
+
+
+print(b)
+print(type(b))
+
+print(c)
+print(type(c))
 
 
