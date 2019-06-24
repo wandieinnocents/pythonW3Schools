3ab012d4bc94fce0188bfaedfe67eb280ca3e930
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-24 15:02:39 +0300

    turples...

---
+++ Turples.py
@@ -0,0 +1,11 @@
+#turples
+
+thistuple = ("apple", "banana", "cherry")
+print(len(thistuple))
+
+#you-cannot itesm to a turple
+
+#you cannot add items to a turple and you cannot remove items in turple
+
+
+
