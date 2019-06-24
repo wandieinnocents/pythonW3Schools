f41da4f9bc7253a715ba6d2fc05a6910732a3930
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-24 14:22:04 +0300

    looping in a list

--- Lists.py
+++
@@ -1,7 +0,0 @@
-#pythonLists
-
-#lists = collections are changable and ordered
-#turple = collections which are ordered and unchangable
-#set = unordered and un-indexed collection
-#dictionary = unordered , changable and indexed
-
---
+++ Lists_dictionary_turples.py
@@ -0,0 +1,23 @@
+#pythonLists
+
+#lists = collections are changable and ordered
+#turple = collections which are ordered and unchangable
+#set = unordered and un-indexed collection
+#dictionary = unordered , changable and indexed
+
+#lists
+myList = ["wandie","innocent","matovu","ronald","julius"]
+print(myList)
+
+#accessing items in list
+#second element array [1]
+print(myList[1])
+
+#change item in list
+myList[0] = "kavuma"
+print(myList)
+
+#looping in list.
+#prints all items in myList
+for x in myList:
+    print(x)
