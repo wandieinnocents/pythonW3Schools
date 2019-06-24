63c4c98f80140587bf4179c08cbef7431e7b9694
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-24 14:17:54 +0300

    lists , turples , dictionaries

---
+++ Lists.py
@@ -0,0 +1,7 @@
+#pythonLists
+
+#lists = collections are changable and ordered
+#turple = collections which are ordered and unchangable
+#set = unordered and un-indexed collection
+#dictionary = unordered , changable and indexed
+
