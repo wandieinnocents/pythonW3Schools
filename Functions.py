b06f199497a78634300d70df91ef66afdcba0a2d
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-26 14:54:53 +0300

    writing and calling functions ..

---
+++ Functions.py
@@ -0,0 +1,14 @@
+#functions in python
+
+#writing the function
+def func():
+    print("This is a function ")
+
+
+def main():
+    #calling a function
+    func()
+
+
+#block of class main
+if __name__ == '__main__':main()
