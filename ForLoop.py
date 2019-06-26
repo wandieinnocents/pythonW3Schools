3368f8c69b6cdf0726273fe316df85103772c343
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-26 14:17:11 +0300

    range of numbers

--- ForLoop.py
+++ ForLoop.py
@@ -5,3 +5,25 @@ fruits = ["apple", "banana", "cherry"]
 for x in fruits:
     print(x)
 
+#looping throuth a world
+
+word = "banana"
+for w in word:
+    print(w)
+
+#using the break statement
+
+names = ["wandie","innocent","kavuma"]
+for position in names:
+
+    if(position == "innocent"):
+        continue
+    print(position)
+
+#range of 4
+for x in range(4):
+    print(x)
+
+#range of 10
+for y in range(10):
+    print(y)
