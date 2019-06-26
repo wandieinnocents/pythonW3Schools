f5955f5a9a5f0d04330efd8625dc4910e60635c4
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-26 15:11:46 +0300

    writing a looping function and call * in programme!

--- Functions.py
+++ Functions.py
@@ -4,15 +4,40 @@
 def func():
     print("This is a function ")
 
+#using two parameters
 def callNames(fname,lname):
     print(fname + lname + " Innocent")
 
+#using default values
+def defValues(id = "student"):
+    print("Student identity is : " + id)
 
+#writing a looping function
+def loopingValues(values):
+    for x in values:
+        print(x)
 
+#function main declaration
 def main():
     #calling a function
     func()
     callNames("wandie"," opio")
+    print("using default function values")
+    defValues("admin")
+
+
+    defValues()
+
+    #looping values
+    print("looping values")
+    fruits = ["mangoes","oranges","pineapples"]
+    print(fruits)
+
+    names = ["wandie","innocent","kavuma"]
+    print(names)
+
+
+
 
 
 #block of class main
