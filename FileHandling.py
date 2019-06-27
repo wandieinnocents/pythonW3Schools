d3925d456d797beef46e163499a8b46658712e62
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-27 10:25:22 +0300

    read first 5 characters in a file

---
+++ FileHandling.py
@@ -0,0 +1,24 @@
+# "r" - Read - Default value. Opens a file for reading, error if the file does not exist
+#
+# "a" - Append - Opens a file for appending, creates the file if it does not exist
+#
+# "w" - Write - Opens a file for writing, creates the file if it does not exist
+#
+# "x" - Create - Creates the specified file, returns an error if the file exists
+#
+# "t" - Text - Default value. Text mode
+#
+# "b" - Binary - Binary mode (e.g. images)
+
+
+#open file for reading
+
+# f = open("file_open.txt")
+#open
+f = open("demo.txt","r")
+#read
+print(f.read())
+
+#read specific char
+chara = open("demo.txt","r")
+print(chara.read(5))
