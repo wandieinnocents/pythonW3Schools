75e9f86ca0a5f5dd3ae94ecf8488d0aba4ff6fbf
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-27 10:35:36 +0300

    removing_deleting files

--- FileHandling.py
+++ FileHandling.py
@@ -9,7 +9,7 @@
 # "t" - Text - Default value. Text mode
 #
 # "b" - Binary - Binary mode (e.g. images)
-
+import os
 
 #open file for reading
 
@@ -22,3 +22,17 @@ print(f.read())
 #read specific char
 chara = open("demo.txt","r")
 print(chara.read(5))
+
+#appending items to a File
+append_data = open("demo.txt","a")
+append_data.write("We are the men")
+append_data.close()
+
+print("Reading data from new appended file")
+read_append = open("demo.txt","r")
+print(read_append.read())
+
+#creating a new file
+f = open("myfile.txt", "x")
+
+os.remove("myfile.txt")
