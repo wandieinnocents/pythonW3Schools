654527410bffd28f92191226a23fc80194b0316e
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-27 09:32:32 +0300

    ..

--- RegularExpressions.py
+++ RegularExpressions.py
@@ -9,3 +9,20 @@ if(look_words):
     print("Words are found man")
 else:
     print("no words matching")
+
+
+#working with findall function
+#returns a list containing all matches
+
+other_word = "The rain is in Spain an aisha"
+match_word = re.findall("ai",other_word)
+print(match_word)
+
+#searching
+search_it = "The man in the hood"
+find = re.search("mand",search_it)
+
+if(find):
+    print("word is found")
+else:
+    print("word is not found")
