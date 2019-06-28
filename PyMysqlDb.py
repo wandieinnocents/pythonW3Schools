0e77e1f8a1ca43213bbc23b2731868ae477cecea
wandieinnocents <wandieinnocent2@gmail.com>
2019-06-28 11:35:39 +0300

    create mysql database

---
+++ PyMysqlDb.py
@@ -0,0 +1,22 @@
+#woring with mysql database - python
+
+#to install mysql type
+#python -m pip install mysql-connector | in your terminal
+
+#import mysql connector for database operations
+import  mysql.connector
+
+
+#establish a connection
+
+mydb = mysql.connector.connect(
+  host="localhost",
+  user="root",
+  passwd="wandie22"
+)
+
+mycursor = mydb.cursor()
+
+#create database
+mycursor.execute("CREATE DATABASE w3schools")
+
